3fefc06ba13fe7b3af91ff999b35ecfbfc5e9170
Ross L. Owen <ross.lloyd.owen@gmail.com>
2024-06-17 11:36:05 -0600

    week2: bom building

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -2,11 +2,24 @@ const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
 const list = document.querySelector('#list');
 
-const li = document.createElement('li');
-li.textContent = input.value;
 
-const deleteBtn = document.createElement('button')
-deleteBtn.textContent = '❌'
+function addChapter() {
+    if (input.value.trim() !== '') {
+        let li = document.createElement('li');
+        li.textContent = input.value;
+        let deleteBtn = document.createElement('button')
+        deleteBtn.textContent = '❌'
+        deleteBtn.addEventListener("click", function () {
+            list.removeChild(li);
+            input.focus();
+        })
+        
+        li.appendChild(deleteBtn);
+        list.appendChild(li);
 
-li.appendChild(deleteBtn);
-list.appendChild(li);
+        input.value = '';
+    }
+    input.focus();
+}
+
+button.addEventListener("click", addChapter);
